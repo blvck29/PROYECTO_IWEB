c20fb54b8585bb9433e456ed22da777ddf5d8814
SergioABS0813 <134556600+SergioABS0813@users.noreply.github.com>
2023-10-22 12:12:37 -0500

    act db
    adt db

---
+++ datos_tablas/proyectoweb_registro_donaciones0.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: proyectoweb
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `registro_donaciones`
+--
+
+DROP TABLE IF EXISTS `registro_donaciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `registro_donaciones` (
+  `idRegistro_Donaciones` int NOT NULL AUTO_INCREMENT,
+  `idUsuario` int NOT NULL,
+  `comprobante` longblob,
+  `monto` double NOT NULL DEFAULT '0',
+  `comprobado` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`idRegistro_Donaciones`),
+  KEY `fk_Registro_Donaciones_Usuarios1_idx` (`idUsuario`),
+  CONSTRAINT `fk_Registro_Donaciones_Usuarios1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `registro_donaciones`
+--
+
+LOCK TABLES `registro_donaciones` WRITE;
+/*!40000 ALTER TABLE `registro_donaciones` DISABLE KEYS */;
+INSERT INTO `registro_donaciones` VALUES (1,106,NULL,20.5,1),(2,102,NULL,54,0),(3,105,NULL,155,1);
+/*!40000 ALTER TABLE `registro_donaciones` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-22 12:12:08
