dab5c14e3c5c397e7efe24d3ff47cc7aecb3da46
Pierohc <133154904+Pierohc@users.noreply.github.com>
2023-10-23 13:01:00 -0500

    dao, servlet usuario

---
+++ proyectoweb/src/main/java/com/example/proyectoweb/model/beans/Usuario.java
@@ -0,0 +1,115 @@
+package com.example.proyectoweb.model.beans;
+
+import java.text.DateFormat;
+
+public class Usuario {
+
+    private int idUsuario;
+    private int idRol;
+    private int idEstado;
+    private String nombres;
+    private String apellidos;
+    private String codigo;
+    private String correo;
+    private String contrasena;
+    private String fechaCreacion;
+    private String ultimoLogin;
+    private String cantEventsInscrito;
+    private String motivoRechazo;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(int idRol) {
+        this.idRol = idRol;
+    }
+
+    public int getIdEstado() {
+        return idEstado;
+    }
+
+    public void setIdEstado(int idEstado) {
+        this.idEstado = idEstado;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(String fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public String getUltimoLogin() {
+        return ultimoLogin;
+    }
+
+    public void setUltimoLogin(String ultimoLogin) {
+        this.ultimoLogin = ultimoLogin;
+    }
+
+    public String getCantEventsInscrito() {
+        return cantEventsInscrito;
+    }
+
+    public void setCantEventsInscrito(String cantEventsInscrito) {
+        this.cantEventsInscrito = cantEventsInscrito;
+    }
+
+    public String getMotivoRechazo() {
+        return motivoRechazo;
+    }
+
+    public void setMotivoRechazo(String motivoRechazo) {
+        this.motivoRechazo = motivoRechazo;
+    }
+}
