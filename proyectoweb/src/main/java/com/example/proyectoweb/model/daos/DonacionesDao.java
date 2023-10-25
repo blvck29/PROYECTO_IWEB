f6a032031253df1975a105a75216f0bf2e4c9f44
blvck29 <a20210850@pucp.edu.pe>
2023-10-25 16:41:48 -0500

    aa

--- proyectoweb/src/main/java/com/example/proyectoweb/model/daos/DonacionesDao.java
+++ proyectoweb/src/main/java/com/example/proyectoweb/model/daos/DonacionesDao.java
@@ -1,6 +1,8 @@
 package com.example.proyectoweb.model.daos;
 
+import com.example.proyectoweb.model.beans.DonacionAlumno;
 import com.example.proyectoweb.model.beans.Donaciones;
+import com.mysql.cj.jdbc.Blob;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -35,9 +37,48 @@ public class DonacionesDao {
                 listaDonaciones.add(donaciones);
             }
 
-        } catch (SQLException e){
-            e.printStackTrace();
+        }catch(SQLException e){
+            throw new RuntimeException(e);
         }
+
         return listaDonaciones;
+    }
+
+
+    public ArrayList<DonacionAlumno> getTablaDonaciones(){
+        ArrayList<DonacionAlumno> listaDonacionesAlumno = new ArrayList<>();
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            throw new RuntimeException(e);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/proyectoweb";
+        String user = "root";
+        String pass = "root";
+        String sql = "SELECT don.idUsuario, don.idRegistro_Donaciones, u.nombres, u.apellidos, don.comprobante, don.monto, don.comprobado FROM registro_donaciones don INNER JOIN usuarios u on (u.idUsuario = don.idUsuario)";
+
+        try(Connection conn = DriverManager.getConnection(url,user,pass);
+            Statement stmt = conn.createStatement();){
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()){
+                DonacionAlumno donacionAlumno = new DonacionAlumno();
+                donacionAlumno.setIdUsuario(rs.getInt(1));
+                donacionAlumno.setIdDonaciones(rs.getInt(2));
+                donacionAlumno.setNombres(rs.getString(3));
+                donacionAlumno.setApellidos(rs.getString(4));
+                donacionAlumno.setComprobante((Blob) rs.getBlob(5));
+                donacionAlumno.setMonto(rs.getDouble(6));
+                donacionAlumno.setComprobado(rs.getBoolean(7));
+
+                listaDonacionesAlumno.add(donacionAlumno);
+            }
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+        return listaDonacionesAlumno;
     }
 }
