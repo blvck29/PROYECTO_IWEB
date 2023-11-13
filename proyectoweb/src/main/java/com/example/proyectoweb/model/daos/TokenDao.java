e8f8fae5f9eb15ba8b1f2640f7b97098343859b3
blvck29 <a20210850@pucp.edu.pe>
2023-11-13 12:27:34 -0500

    Update TokenDao.java

--- proyectoweb/src/main/java/com/example/proyectoweb/model/daos/TokenDao.java
+++ proyectoweb/src/main/java/com/example/proyectoweb/model/daos/TokenDao.java
@@ -6,24 +6,20 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class TokenDao {
+public class TokenDao extends DaoBase{
 
     public void generate(String idUsuario){
 
         String token = RandomTokenGenerator.generator();
 
-
-        String sql = "INSERT INTO `proyectoweb`.`token_generado` (`location_id`, `street_address`, `postal_code`, `city`, `state_province`, `country_id`) VALUES (?, ?, ?, ?, ?, ?);";
+        String sql = "INSERT INTO `proyectoweb`.`token_generado` (`idUsuario`, `token`) VALUES (?, ?);";
 
         try(Connection conn = getConnection();
             PreparedStatement pstmt = conn.prepareStatement(sql)){
 
-            pstmt.setString(1, locationId);
-            pstmt.setString(2, streetAddress);
-            pstmt.setString(3, postalCode);
-            pstmt.setString(4, city);
-            pstmt.setString(5, stateProvince);
-            pstmt.setString(6, countryId);
+            pstmt.setString(1, idUsuario);
+            pstmt.setString(2, token);
+
 
             pstmt.executeUpdate();
 
