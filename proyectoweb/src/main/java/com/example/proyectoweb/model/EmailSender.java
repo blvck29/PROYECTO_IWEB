7e4e3f20fba48eaac6527c0dc9e1db5c3fe9e19e
blvck29 <a20210850@pucp.edu.pe>
2023-12-06 10:10:56 -0500

    EmailSender

---
+++ proyectoweb/src/main/java/com/example/proyectoweb/model/EmailSender.java
@@ -0,0 +1,41 @@
+package com.example.proyectoweb.model;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class EmailSender {
+
+    public static void sendEmail(String toEmail, String subject, String body) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        final String username = "fibratoxica.sdi@gmail.com";
+        final String password = "acceso123ah@";
+        Session session = Session.getInstance(props, new Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+            message.setSubject(subject);
+            message.setText(body);
+
+            Transport.send(message);
+
+            System.out.println("Correo enviado exitosamente");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
