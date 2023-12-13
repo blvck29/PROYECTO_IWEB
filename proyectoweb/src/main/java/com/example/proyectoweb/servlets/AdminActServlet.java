56f3e8e4bf6c252456ba10e693f728d35bb5940a
Pierohc <pieritohc@hotmail.com>
2023-12-12 23:22:18 -0500

    aa

--- proyectoweb/src/main/java/com/example/proyectoweb/servlets/AdminActServlet.java
+++ proyectoweb/src/main/java/com/example/proyectoweb/servlets/AdminActServlet.java
@@ -144,10 +144,23 @@ public class AdminActServlet extends HttpServlet {
                 case "album":
                     String idEventoAlbum = request.getParameter("idEvento");
                     Evento eventoAlbum = eventoDao.buscarEventoId(idEventoAlbum);
-                    ArrayList<Integer> idsFotosAlbum = albumDao.idFotosAlbum(idEventoAlbum);
-                    request.setAttribute("listaIds", idsFotosAlbum);
-                    request.setAttribute("evento", eventoAlbum);
-                    request.getRequestDispatcher("pages/admin_act/upload_album.jsp").forward(request, response);
+
+                    if(eventoAlbum!=null){
+                        if (eventoAlbum.getIdEstado().equals("FINISH")) {
+                            ArrayList<Integer> idsFotosAlbum = albumDao.idFotosAlbum(idEventoAlbum);
+                            request.setAttribute("listaIds", idsFotosAlbum);
+                            request.setAttribute("evento", eventoAlbum);
+                            request.getRequestDispatcher("pages/admin_act/upload_album.jsp").forward(request, response);
+                        }else{
+                            response.sendRedirect(request.getContextPath() + "/admin_act?action=home");
+                        }
+                    }else{
+                        response.sendRedirect(request.getContextPath() + "/admin_act?action=home");
+                    }
+
+
+
+
                     break;
 
                 case "imagenDeAlbum":
